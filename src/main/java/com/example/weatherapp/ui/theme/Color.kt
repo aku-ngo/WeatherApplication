a972bb8187522f65049db8aae326630066305ac4
aku-ngo <135605676+aku-ngo@users.noreply.github.com>
2023-06-11 21:41:48 +0300

    Add files via upload

---
+++ src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val lightgray = Color(0xd3d3d3)
+val Gray = Color(0x808080)
+val verylightgrey = Color(0xdcdcdc)
+val darkblue = Color(0X06038D)
+val yellow = Color(0xffff00)
